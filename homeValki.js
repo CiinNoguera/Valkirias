ad9ef7d16f1ba5f8223571680e79dd793a7d869f
Cintia Noguera <ciin.noguera@hotmail.com>
2024-01-03 17:27:37 -0300

    first mobile

---
+++ homeValki.js
@@ -0,0 +1,4 @@
+const btnAtras = document.querySelector(".carrusel__btnAtras");
+const btnSiguiente = document.querySelector(".carrusel__btnSiguiente");
+const imgInsta = document.querySelector(".carrusel__img");
+const imgCel = document.querySelector(".carrusel__imgCel");
